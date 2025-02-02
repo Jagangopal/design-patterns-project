0eb8a0daaae4027f5bd09303f88b074d39b8ffd2
Jagan Gopal Pattabiraman <pjagangopal@gmail.com>
2025-02-02 18:03:02 +0530

    template design pattern - behavioral

---
+++ src/main/java/com/design/patterns/behavourial/template/HousingClient.java
@@ -0,0 +1,16 @@
+package com.design.patterns.behavourial.template;
+
+public class HousingClient {
+    public static void main(String[] args) {
+
+        HouseTemplate houseType = new WoodenHouse();
+
+        //using template method
+        houseType.buildHouse();
+        System.out.println("************");
+
+        houseType = new GlassHouse();
+
+        houseType.buildHouse();
+    }
+}
